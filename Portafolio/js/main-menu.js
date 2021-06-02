a8b131f039f37868090a373f69e84af5e4e37804
Gonzalo <fgvr92@gmail.com>
2021-06-02 02:21:23 -0500

    Se agrega bars de fontaswome y se agregan estilos y codigo js para menu responsive.

---
+++ Portafolio/js/main-menu.js
@@ -0,0 +1,31 @@
+// OBTENER ELEMENTOS DE HTML
+let responsiveMenuBtn = document.getElementById("responsive-menu-btn");
+let mainMenu = document.getElementById("main-menu");
+
+//PARA CONTROLAR EL MENU
+let flagMenu = false;
+
+//FUNCIONES
+
+function toggleMenu() {
+  //   mainMenu.setAttribute("class", "open-menu-responsive");
+
+  //Si el flag no existe, podemos decir que el menú está cerrado
+  if (flagMenu === false) {
+    mainMenu.classList.add("open-menu-responsive");
+    // mainMenu.setAttribute("class", "open-menu-responsive");
+
+    flagMenu = true;
+  }
+  //Si el flag existe, podemos decir que el menú está abierto
+  else {
+    mainMenu.classList.remove("open-menu-responsive");
+    // mainMenu.removeAttribute("class", "open-menu-responsive");
+
+    flagMenu = false;
+  }
+}
+
+// EVENTOS
+
+responsiveMenuBtn.addEventListener("click", toggleMenu);
