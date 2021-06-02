167fc0f83baa1bf965d33cac90304139ed45d669
Gonzalo <fgvr92@gmail.com>
2021-06-02 03:02:20 -0500

    se cambia transicion de aparicion de menu

--- Portafolio/js/main-menu.js
+++ Portafolio/js/main-menu.js
@@ -7,25 +7,25 @@ let flagMenu = false;
 
 //FUNCIONES
 
-function toggleMenu() {
-  //   mainMenu.setAttribute("class", "open-menu-responsive");
 
-  //Si el flag no existe, podemos decir que el menú está cerrado
-  if (flagMenu === false) {
-    mainMenu.classList.add("open-menu-responsive");
-    // mainMenu.setAttribute("class", "open-menu-responsive");
-
-    flagMenu = true;
-  }
-  //Si el flag existe, podemos decir que el menú está abierto
-  else {
-    mainMenu.classList.remove("open-menu-responsive");
-    // mainMenu.removeAttribute("class", "open-menu-responsive");
-
-    flagMenu = false;
-  }
-}
 
 // EVENTOS
 
-responsiveMenuBtn.addEventListener("click", toggleMenu);
+responsiveMenuBtn.addEventListener("click", function() {
+    //   mainMenu.setAttribute("class", "open-menu-responsive");
+  
+    //Si el flag no existe, podemos decir que el menú está cerrado
+    if (flagMenu === false) {
+      mainMenu.classList.add("open-menu-responsive");
+      // mainMenu.setAttribute("class", "open-menu-responsive");
+  
+      flagMenu = true;
+    }
+    //Si el flag existe, podemos decir que el menú está abierto
+    else {
+      mainMenu.classList.remove("open-menu-responsive");
+      // mainMenu.removeAttribute("class", "open-menu-responsive");
+  
+      flagMenu = false;
+    }
+  });
