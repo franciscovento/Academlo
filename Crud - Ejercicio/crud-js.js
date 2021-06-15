dbc3588548ab1c3c700704c9ac61b4ada203903d
Gonzalo <fgvr92@gmail.com>
2021-06-15 18:04:56 -0500

    uso de localstorage para crud

--- Crud - Ejercicio/crud-js.js
+++ Crud - Ejercicio/crud-js.js
@@ -1,19 +1,4 @@
-let cars = [
-    {
-      name: "Mazda 2",
-      model: "2019",
-      doors: 5,
-      color: "red",
-      brand: "mazda"
-    },
-    {
-        name: "Toyota",
-        model: "2019",
-        doors: 5,
-        color: "red",
-        brand: "mazda"
-      }
-  ]
+let cars = JSON.parse(localStorage.getItem("arregloStorage"));
 
 //   DOM
 
@@ -26,10 +11,10 @@ let updateIndex = null;
 
 // función para guardar en localStorage
 
-const userStorage = () => {
+const autosStorage = () => {
   if  (typeof Storage !== "undefined"){
   
-  localStorage.setItem("userStorage", JSON.stringify(cars));
+  localStorage.setItem("arregloStorage", JSON.stringify(cars));
    
   } else {
     alert("Tu navegador no es compatible con este almacenamiento");
@@ -39,48 +24,79 @@ const userStorage = () => {
 
 const renderTarjetas = arreglo => {
 
-    containerTarjetasUI.innerHTML = "";
-    let keysArreglo = Object.keys(arreglo[0]);
+      containerTarjetasUI.innerHTML = "";
+      //let keysArreglo = Object.keys(arreglo[0]);
+      //let arreglo = JSON.parse(localStorage.getItem("arregloStorage"));
+      if (arreglo === null) {
+        arreglo = [];
+      } else {
 
-    arreglo.forEach((element, index) => {
-        
-        let Tarjeta = document.createElement("div");
-        Tarjeta.setAttribute("class", "Tarjeta");
-        containerTarjetasUI.appendChild(Tarjeta);
-    
-        let TarjetaDatosUI = document.createElement("div");
-        TarjetaDatosUI.setAttribute("class", "Tarjeta-datos");
-        Tarjeta.appendChild(TarjetaDatosUI);
-    
-        
-        keysArreglo.forEach((element, i) => {
 
-        let h2UI = document.createElement("h2");
-        h2UI.setAttribute("class", "item");
-        TarjetaDatosUI.appendChild(h2UI);
-        
-        h2UI.innerText = element +": " + arreglo[index][element];
-        });
-
-        let tarjetaBtns = document.createElement("div");
-        tarjetaBtns.setAttribute("class", "Tarjeta-Btns");
-        Tarjeta.appendChild(tarjetaBtns);
-
-        let buttonEditar = document.createElement("button");
-        buttonEditar.className = "item-1 btns btns_secondary BtnEditar";
-        buttonEditar.addEventListener("click", () => editarElementoLista(arreglo, index));
-        buttonEditar.id = "BtnEditar";
-        buttonEditar.innerHTML = `<span class="material-icons">edit</span>`
-        tarjetaBtns.appendChild(buttonEditar);
-
-        let buttonEliminar = document.createElement("button");
-        buttonEliminar.className = "item-1 btns BtnEliminar";
-        buttonEliminar.addEventListener("click", () => eliminarElementoLista(arreglo, index));
-        buttonEliminar.id = "BtnEliminar";
-        buttonEliminar.innerHTML = `<span class="material-icons icons-2">delete</span>`
-        tarjetaBtns.appendChild(buttonEliminar);    
-    });
-
+        arreglo.forEach((element, index) => {
+           
+          let Tarjeta = document.createElement("div");
+          Tarjeta.setAttribute("class", "Tarjeta");
+          containerTarjetasUI.appendChild(Tarjeta);
+      
+          let TarjetaDatosUI = document.createElement("div");
+          TarjetaDatosUI.setAttribute("class", "Tarjeta-datos");
+          Tarjeta.appendChild(TarjetaDatosUI);
+      
+        // Agrego elementos manualmente
+          let nameUI = document.createElement("h2");
+          nameUI.setAttribute("class", "item");
+          TarjetaDatosUI.appendChild(nameUI);
+          nameUI.innerText = "name: " + element.name;
+  
+          let modelUI = document.createElement("h2");
+          modelUI.setAttribute("class", "item");
+          TarjetaDatosUI.appendChild(modelUI);
+          modelUI.innerText = "model: " + element.model;
+  
+          let doorsUI = document.createElement("h2");
+          doorsUI.setAttribute("class", "item");
+          TarjetaDatosUI.appendChild(doorsUI);
+          doorsUI.innerText = "doors: " + element.doors;
+  
+          let colorUI = document.createElement("h2");
+          colorUI.setAttribute("class", "item");
+          TarjetaDatosUI.appendChild(colorUI);
+          colorUI.innerText = "color: " + element.color;
+  
+          let brandUI = document.createElement("h2");
+          brandUI.setAttribute("class", "item");
+          TarjetaDatosUI.appendChild(brandUI);
+          brandUI.innerText = "marca: " + element.brand;
+  
+  
+          // keysArreglo.forEach((element, i) => {
+  
+          // let h2UI = document.createElement("h2");
+          // h2UI.setAttribute("class", "item");
+          // TarjetaDatosUI.appendChild(h2UI);
+          
+          // h2UI.innerText = element +": " + arreglo[index][element];
+          // });
+  
+          let tarjetaBtns = document.createElement("div");
+          tarjetaBtns.setAttribute("class", "Tarjeta-Btns");
+          Tarjeta.appendChild(tarjetaBtns);
+  
+          let buttonEditar = document.createElement("button");
+          buttonEditar.className = "item-1 btns btns_secondary BtnEditar";
+          buttonEditar.addEventListener("click", () => editarElementoLista(arreglo, index));
+          buttonEditar.id = "BtnEditar";
+          buttonEditar.innerHTML = `<span class="material-icons">edit</span>`
+          tarjetaBtns.appendChild(buttonEditar);
+  
+          let buttonEliminar = document.createElement("button");
+          buttonEliminar.className = "item-1 btns BtnEliminar";
+          buttonEliminar.addEventListener("click", () => eliminarElementoLista(arreglo, index));
+          buttonEliminar.id = "BtnEliminar";
+          buttonEliminar.innerHTML = `<span class="material-icons icons-2">delete</span>`
+          tarjetaBtns.appendChild(buttonEliminar);    
+      });
+      }
 }
 
 const agregarElementoLista = event => {
@@ -103,6 +119,7 @@ const agregarElementoLista = event => {
     updateFlag = false;
     updateIndex = null;
     renderTarjetas(cars);
+    autosStorage();
     AgregarElemento.reset();
 
     } else {
@@ -115,6 +132,7 @@ const agregarElementoLista = event => {
         };
         cars.push(elemento);
         renderTarjetas(cars);
+        autosStorage();
         AgregarElemento.reset();
     }
     
@@ -123,6 +141,7 @@ const agregarElementoLista = event => {
 
 const eliminarElementoLista = (arreglo, index) => {
     arreglo.splice(index,1);
+    autosStorage();
     renderTarjetas(cars);
   }
 
@@ -142,6 +161,4 @@ const editarElementoLista = (arreglo, index) => {
 renderTarjetas(cars);
 AgregarElemento.addEventListener("submit", agregarElementoLista);
 
-
- 
 
